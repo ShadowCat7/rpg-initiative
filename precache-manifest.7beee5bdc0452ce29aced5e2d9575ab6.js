688efa849cef8043ee32ab05a295fb77e40e8ed3
ShadowCat7 <lukasattridge@gmail.com>
2019-08-09 18:27:55 -0700

    Updates

---
+++ precache-manifest.7beee5bdc0452ce29aced5e2d9575ab6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50c4ed73553016628a5415e67406b89f",
+    "url": "/rpg-initiative/index.html"
+  },
+  {
+    "revision": "38ada46d501003dc7fde",
+    "url": "/rpg-initiative/static/css/main.c54b00d3.chunk.css"
+  },
+  {
+    "revision": "6fa768f77802ce7b7967",
+    "url": "/rpg-initiative/static/js/2.3890d954.chunk.js"
+  },
+  {
+    "revision": "38ada46d501003dc7fde",
+    "url": "/rpg-initiative/static/js/main.1f2c3025.chunk.js"
+  },
+  {
+    "revision": "7ba57d40703cf0fd8f8a",
+    "url": "/rpg-initiative/static/js/runtime~main.90666d56.js"
+  }
+]);
